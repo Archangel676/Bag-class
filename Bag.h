b761309cf27b41023a55220d2fc5a15c93dc79ab
Maya <mvenega9@my.chemeketa.edu>
2024-05-19 01:58:50 +0000

    May 18, 2024, 6:58 PM

--- Bag.h
+++ Bag.h
@@ -1,4 +1,36 @@
 #ifndef BAG_H
 #define BAG_H
 
+template<typename T>
+
+class Bag {
+private:
+  int capacity;
+  T* bagArray = nullptr;
+
+public:
+  Bag()
+  {
+    capacity = 10;
+    bagArray = new T[capacity];
+  }
+
+  ~Bag()
+  {
+    delete[] bagArray;
+  }
+
+  Bag(const Bag& other)
+  {
+    capacity = other.capacity;
+    bagArray = new T[capacity];
+
+    for (int i = 0; i < capacity; i++) {
+      bagArray[i] = other.bagArray[i];
+    }
+  }
+
+  Copy constructor, Assignment Operator
+};
+
 #endif
